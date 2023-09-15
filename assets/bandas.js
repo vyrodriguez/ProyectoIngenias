a7ada059cadc958c23dbd1b6943ff75e669d6030
vyrodriguez <anavictoriarodriguez94@gmail.com>
2023-09-14 21:01:53 -0300

    Se agregaron funciones de ingreso y registro

--- assets/bandas.js
+++ assets/bandas.js
@@ -1,9 +1,14 @@
-// function mostrarAlerta() {
-//     alert('¡BIENVENIDOS A BANDAS COMODORENSES, NO TE OLVIDES DE SEGUIRNOS EN NUESTRAS REDES SOCIALES!');
-// }
+ function mostrarAlerta() {
+     alert('¡BIENVENIDOS A BANDAS COMODORENSES, NO TE OLVIDES DE SEGUIRNOS EN NUESTRAS REDES SOCIALES!');
+ }
 
 
 function iniciarSesion() {
     const usuario = prompt("Ingrese su nombre de usuario:");
     const contrasena = prompt("Ingrese su contraseña:");
+    alert('Inicio de sesion exitoso')
+}
+
+function Registrarse() {
+    alert('Sin Habilitar, sepan disculpar')
 }
