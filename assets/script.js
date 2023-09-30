8a4fc1571e6129e098dec49b58eda52aad96f701
julygarcia12 <aleyulianagarcia@gmail.com>
2023-09-30 19:39:46 -0300

    cambios y formato

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+// JSON
+ const banda1json = {
+    
+   "nombre":"abelardos",
+    "imagen":"src=img/invitaciondeabelardos.jpg",
+    "fecha":"10 de diciembre",
+    "ubicacion":"San Martin 239, Comodoro Rivadavia",
+    "entradas":"calle san martin 239",
+ 
+
+}
+ 
+const banda2json = {
+   
+      "nombre":"aluxinados",
+      "imagen":"src=img/invitaciondealuxinados.jpg",
+      "fecha":"20 de enero",
+      "ubicacion":"San Martin 239, Comodoro Rivadavia",
+      "entradas":"calle san martin 239",
+   
+}
+
+const banda3json = {
+
+   "nombre":"el descarte",
+   "imagen": "src=imag/invitaciondeeldescarte",
+   "fecha": "1 de octubre",
+   "ubicacion": "San Martin 464, Comodoro Rivadavia",
+   "entradas":"Calle San Martin 239",
+
+}
+
+const banda4json = {
+
+   "nombre":"Dios te Salve Vecina",
+   "imagen": "src=img/invitaciondediostesalve",
+   "fecha":"11 de marzo",
+   "ubicacion":"San Martin 239, Comodoro Rivadavia",
+   "entradas": "San Martin 239",
+
+}
+
+const banda5json = {
+   "nombre":"Polifonia",
+   "imagen":"src=img/invitciondepolifonia",
+   "fecha":"8 de julio",
+   "ubicacion":"San Martin 239, Comodoro Rivadavia",
+   "entradas":"Sarmiento 923"
+
+}
